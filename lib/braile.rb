9e16b261b801691c54ee611fc1abf1d5fde8172f
Yesi Meza <yesi.meza10@gmail.com>
2020-11-08 12:47:16 -0700

    created get alphabet test and method

--- lib/braile.rb
+++ lib/braile.rb
@@ -1,3 +1,11 @@
 class Braile
 
-end 
+  def initialize
+    @alphabet = Alphabet.new
+  end
+
+  def get_braile(letter)
+    @alphabet.letters[letter]
+    # require "pry"; binding.pry
+  end
+end
--- test/braile_test.rb
+++ test/braile_test.rb
@@ -1,12 +1,22 @@
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/braile'
+require './lib/alphabet'
 
 
 class BraileTest < Minitest::Test
  def test_it_exists
    braile = Braile.new
 
-   assert_instance_of Braile, braile 
+   assert_instance_of Braile, braile
+ end
+
+ def test_it_can_return_a_letter_of_alphabet
+    braile = Braile.new
+    alphabet = Alphabet.new
+
+    expected = "0....."
+
+    assert_equal expected, braile.get_braile("a")
  end
 end
