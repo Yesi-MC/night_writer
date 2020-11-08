6f9ad1af48c39b4bec32acefe9c8402542a4c715
Yesi Meza <yesi.meza10@gmail.com>
2020-11-08 14:36:44 -0700

    in the process of creating a method to format words

--- lib/braille.rb
+++ lib/braille.rb
@@ -15,6 +15,13 @@ class Braille
     row1 << get_braille(letter).chars[0..1].join
     row2 << get_braille(letter).chars[2..3].join
     row3 << get_braille(letter).chars[4..5].join
-    "#{row1}\n " + "#{row2}\n " + "#{row3}"
+    "#{row1}\n" + "#{row2}\n" + "#{row3}"
+  end
+
+  def format_words(words)
+    words.chars.map do |letter|
+      format_letter(letter)
+      # require "pry"; binding.pry
+    end
   end
 end
