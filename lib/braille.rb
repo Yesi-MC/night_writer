42664399fb2cf0f36dc64777737f53d1d8f55d7f
Yesi Meza <yesi.meza10@gmail.com>
2020-11-08 19:21:11 -0700

    removed the pry and other notes

--- lib/braille.rb
+++ lib/braille.rb
@@ -25,14 +25,9 @@ class Braille
     row2 = []
     row3 = []
     words.chars.each do |letter|
-      # require "pry"; binding.pry
-      # if letter != nil
       row1 << get_braille(letter)[0..1]
       row2 << get_braille(letter)[2..3]
       row3 << get_braille(letter)[4..5]
-    # else
-    #   return
-      # require "pry"; binding.pry
     end
     "#{row1.join}" << "\n" << "#{row2.join}" << "\n" << "#{row3.join}" << "\n"
   end
