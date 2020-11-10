1eeade5c92bcd5d80ab16d3a1d626e86391af460
Yesi Meza <yesi.meza10@gmail.com>
2020-11-09 20:48:18 -0700

    created method for return_english and passed

--- lib/unbraille.rb
+++ lib/unbraille.rb
@@ -12,8 +12,34 @@ class Unbraille
     @braille_alphabet.braille_letters[remove_space]
   end
 
-  # def return_letter(braille)
-  #   get_english(braille)
-  #   require "pry"; binding.pry
+  def count_of_letters(braille)
+    braille.split[0].length / 2
+  end
+
+  def return_english(braille)
+    braille_in_order = []
+    split_line = braille.split("\n")
+    count_of_letters(braille).times do
+      split_line.each do |line|
+        braille_in_order << line.slice!(0..1)
+      end
+    end
+    array = braille_in_order.join.scan(/(..)(..)(..)/)
+    sentence = []
+    array.each do |char|
+      sentence << get_english(char.join)
+    end
+    sentence.join
+    # require "pry"; binding.pry
+  end
+
+  # def join_braille(braille)
+  #
+  #   count_of_letters(braille).times do
+  #     letter = braille[0..2].join
+  #   sentence << get_english(letter)
+  #   end
+  #   sentence
   # end
+
 end
