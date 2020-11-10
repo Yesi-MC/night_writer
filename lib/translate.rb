74464cc5a0aef9ace3513be47300a747d1b72b62
Yesi Meza <yesi.meza10@gmail.com>
2020-11-09 20:58:18 -0700

    created return_english method

--- lib/translate.rb
+++ lib/translate.rb
@@ -1,12 +1,19 @@
 require './lib/braille'
+require './lib/unbraille'
+
 
 class Translate
 
   def initialize
     @braille = Braille.new
+    @unbraille = Unbraille.new
   end
 
   def translate_to_braille(message)
     @braille.format_messages(message)
+  end
+
+  def translate_to_english(message)
+    @unbraille.return_english(message)
   end
 end
