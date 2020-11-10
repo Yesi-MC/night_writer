d2cbfe680d924ec988b9d805c02ccabb31449372
Yesi Meza <yesi.meza10@gmail.com>
2020-11-10 00:12:38 -0700

    removed unnecessary variable

--- test/braille_test.rb
+++ test/braille_test.rb
@@ -12,7 +12,6 @@ class BrailleTest < Minitest::Test
 
  def test_it_can_return_a_letter_of_alphabet
     braille = Braille.new
-    alphabet = Alphabet.new
 
     expected = "0....."
 
@@ -21,7 +20,6 @@ class BrailleTest < Minitest::Test
 
  def test_it_can_format_letter
     braille = Braille.new
-    alphabet = Alphabet.new
 
     expected = "0.\n..\n.."
 
@@ -30,7 +28,6 @@ class BrailleTest < Minitest::Test
 
  def test_it_can_format_words
    braille = Braille.new
-   alphabet = Alphabet.new
 
    expected =
     "0.0.0.0.0....00.0.0.00\n00.00.0..0..00.0000..0\n....0.0.0....00.0.0...\n"
@@ -40,7 +37,6 @@ class BrailleTest < Minitest::Test
 
  def test_it_knows_special_characters
    braille = Braille.new
-   alphabet = Alphabet.new
 
    expected =
    ".00.0.0.0.0....00.0.0.00..\n.000.00.0..0..00.0000..000\n00....0.0.0....00.0.0...0.\n"
@@ -50,7 +46,6 @@ class BrailleTest < Minitest::Test
 
  def test_it_cant_be_longer_than_40_characters
    braille = Braille.new
-   alphabet = Alphabet.new
 
    expected = ".00...000.00...0..000.0.0...00.0.0.00.0.0..0.....00..0.0..0.0.000000..0..0.0.0\n00.0...0.0.0..00.........0....0.0.00.....00.0...00..0.00..00..0.0..0..0.0.0000\n.0......0.0.0.0...0...0.....0...0.0...0...0.......000.0.......0.0.00..0...0.0.\n0.0...0.0000.0000.00.0.0..\n0..0........0..0.0.0000.00\n0.................0.0.0.0.\n"
 
